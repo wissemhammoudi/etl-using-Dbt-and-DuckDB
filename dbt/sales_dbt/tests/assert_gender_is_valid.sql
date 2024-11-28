42a0723dd8599481479391037d1b318b5ed29673
feee08 <111534962+wisemhm@users.noreply.github.com>
2024-11-28 14:14:56 +0100

    last

--- dbt/sales_dbt/tests/assert_gender_is_valid.sql
+++
@@ -1,6 +0,0 @@
--- tests/assert_gender_is_valid.sql
-SELECT
-    customer_id,
-    gender
-FROM {{ ref('dim_customer') }}  -- Update with your actual model name
-WHERE gender NOT IN ('Female', 'Male')
